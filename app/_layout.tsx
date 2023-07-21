8cc7c16dec3339212b2d32644ff622307a830084
Jamie Taylor <jvptaylor@hotmail.com>
2023-07-20 21:37:30 -0400

    tab styling, native base configured with router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import { NativeBaseProvider, useColorMode, useColorModeValue } from 'native-base';
+import { theme, DarkTheme, DefaultTheme } from '../constants/Theme';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -36,16 +37,15 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-        </Stack>
-      </ThemeProvider>
-    </>
-  );
+ 
+	return (
+		<NativeBaseProvider theme={theme}>    
+			<ThemeProvider value={theme.config.initialColorMode === 'dark' ? DarkTheme : DefaultTheme}>
+				<Stack>
+					<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+					<Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+				</Stack>
+			</ThemeProvider>
+		</NativeBaseProvider>
+	);
 }
---
+++ constants/Theme.ts
@@ -0,0 +1,103 @@
+import { extendTheme, useColorModeValue } from 'native-base'
+import type { Theme } from '@react-navigation/native/src/types';
+
+// Define the config
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+  background: 'transparent'
+};
+
+const colors = {
+	primary: {
+	  50: "#fff5f7",
+	  100: "#ffe6ea",
+	  200: "#ffccd6",
+	  300: "#ffa4b4",
+	  400: "#fe4365",
+	  500: "#e9012c",
+	  600: "#a7011f",
+	  700: "#7a0117",
+	  800: "#47000d",
+	  900: "#240007",
+	  950: "#140004"
+	},
+	secondary: {
+	  50: "#fff5f5",
+	  100: "#fee7e6",
+	  200: "#fdcfce",
+	  300: "#fca9a6",
+	  400: "#fc9d9a",
+	  500: "#e40e07",
+	  600: "#a30a05",
+	  700: "#770804",
+	  800: "#450402",
+	  900: "#230201",
+	  950: "#140101"
+	},
+	tertiary: {
+	  50: "#fef9f6",
+	  100: "#fdf0e7",
+	  200: "#fbe2d0",
+	  300: "#f9cbaa",
+	  400: "#f29654",
+	  500: "#da6410",
+	  600: "#9d480c",
+	  700: "#723409",
+	  800: "#421f05",
+	  900: "#210f02",
+	  950: "#130901"
+	},
+	quat: {
+	  50: "#fbfbf9",
+	  100: "#f5f5ef",
+	  200: "#ebebe0",
+	  300: "#dbdbc7",
+	  400: "#b7b78f",
+	  500: "#8f8f5b",
+	  600: "#676742",
+	  700: "#4b4b30",
+	  800: "#2c2c1c",
+	  900: "#16160e",
+	  950: "#0c0c08"
+	},
+	quint: {
+	  50: "#f9fbfa",
+	  100: "#eff5f3",
+	  200: "#e0ebe6",
+	  300: "#c7dbd2",
+	  400: "#8fb7a5",
+	  500: "#5b8f78",
+	  600: "#426756",
+	  700: "#304b3f",
+	  800: "#1c2c24",
+	  900: "#0e1612",
+	  950: "#080c0a"
+	}
+}
+
+export const DarkTheme: Theme = {
+	dark: true,
+	colors: {
+	  primary: colors.primary[500],
+	  background: 'rgb(1, 1, 1)',
+	  card: 'rgb(18, 18, 18)',
+	  text: colors.secondary[100],
+	  border: 'rgb(39, 39, 41)',
+	  notification: 'rgb(255, 69, 58)',
+	},
+};
+
+export const DefaultTheme: Theme = {
+	dark: false,
+	colors: {
+	  primary: 'rgb(0, 122, 255)',
+	  background: 'rgb(242, 242, 242)',
+	  card: 'rgb(255, 255, 255)',
+	  text: 'rgb(28, 28, 30)',
+	  border: 'rgb(216, 216, 216)',
+	  notification: 'rgb(255, 59, 48)',
+	},
+  };
+
+export const theme = extendTheme({ config, colors });
