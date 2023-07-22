8c2aab52c64e876680ac09fee5c9473ada38c920
Jamie Taylor <jvptaylor@hotmail.com>
2023-07-22 16:18:33 -0400

    FabMenu functionality component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
 import { View, FlatList } from 'native-base'
 import tweets from '../../assets/data/tweets';
 import PostView from '../../components/PostView';
+import FabMenu from '../../components/FabMenu';
+
 
 export default function TabOneScreen() {
 	return (		
-		<View flex={1} >
+		<View flex={1} >		
 			<FlatList data={tweets} renderItem={({item}) => <PostView post={item} />} />
+			<FabMenu />
 		</View>		
 	);
 }
---
+++ app/new-post.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "native-base"
+
+export default function NewPost(){
+    return(
+        <view>
+            <Text>Create new post</Text>
+        </view>
+    )
+}
